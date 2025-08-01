84c0f949c062afa1962efb8c325368745d650e9f
dimasavenok <dimas.savenok1990@gmail.com>
2025-08-01 20:12:19 +0300

    create models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя категории")
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя категории")
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.category} | {self.price}"
--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'first_django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'dima510141',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
