5b950a0d7133b98cdcf6ff80dfad13927dc91555
dimasavenok <dimas.savenok1990@gmail.com>
2025-08-12 20:03:37 +0300

    create blogapp

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from catalog import views
+from catalog.views import HomeView, ContactsView, ProductDetailView
 
 app_name = "catalog"
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
 from catalog.models import Product
 
 
 # Create your views here.
-def home(request):
-    context = {
-        "title": "Skystore",
-        "products": Product.objects.all()
-    }
-    return render(request, "catalog/home.html", context=context)
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        "title": f"Skystore - {product.name}",
-        "product": product
-    }
-    return render(request, "catalog/product.html", context=context)
+class HomeView(ListView):
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Skystore"
+        return context
+
+
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Skystore"
+        return context
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "catalog/product.html"
+    context_object_name = "product"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Skystore"
+        return context
